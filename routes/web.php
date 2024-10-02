e981801b02c2b2da0e92a268a1693c4d695b9103
Siavoosh <siavash.ahmadpoor@gmail.com>
2024-10-02 10:50:30 +0330

    auth middleware problem fixed , still no login route

--- routes/web.php
+++ routes/web.php
@@ -21,44 +21,46 @@ Route::controller(SessionController::class)->group(function(){
     Route::post('/logout','destroy')->name('logout');
 });
 
-Route::prefix('/dashboard')->group(function(){
-    Route::view('/','dashboard.index')->name('dashboard');
-
-    Route::controller(CategoryController::class)->group(function(){
-
-        Route::get('/newcategory','create')->name('dashboard.newcategory');
-        Route::post('/newcategory','store')->name('category.store');
-        Route::get('/categories','index')->name('categories');
-        Route::delete('/categories/{id}','destroy')->name('dashboard.deletecategory');
-    });
-    Route::controller(PostController::class)->group(function(){
-        Route::get('/newpost','create')->name('newpost');
-        Route::post('/newpost','store')->name('storepost');
-        Route::get('/posts','showbyUser')->name('showpostbyuser');
-        Route::delete('/posts/{post}','destroy')->name('deletepost');
-        Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('postedit');
-        Route::put('/posts/{post}', [PostController::class, 'update'])->name('postupdate');
-
-    });
-    Route::controller(PageController::class)->group(function(){
-        Route::get('/newpage','create')->name('newpage');
-        Route::get('/pages','showbyuser')->name('showpagesbyuser');
-        Route::post('/newpage','store')->name('storepage');
-        Route::delete('/pages/{page}','destroy')->name('deletepage');
-        Route::get('/pages/{page}/edit', 'edit')->name('pageedit');
-        Route::put('/pages/{page}', 'update')->name('updatepage');
-
-
-    });
-    Route::get('/comments',[CommentController::class,'index'])->name('managecomments');
-    Route::post('/comments/{comment}',[CommentController::class,'action'])->name('actioncomment');
-
-
-    Route::get('/registeruser',[RegisteredUser::class,'create'])->name('createuser')->middleware('can:admin');
-    Route::post('/registeruser',[RegisteredUser::class,'createtheuser'])->name('createtheuser')->middleware('can:admin');
-
-
-})->middleware('auth');
+Route::middleware('auth')->group(function(){
+    Route::prefix('/dashboard')->group(function(){
+        Route::view('/','dashboard.index')->name('dashboard');
+    
+        Route::controller(CategoryController::class)->group(function(){
+    
+            Route::get('/newcategory','create')->name('dashboard.newcategory');
+            Route::post('/newcategory','store')->name('category.store');
+            Route::get('/categories','index')->name('categories');
+            Route::delete('/categories/{id}','destroy')->name('dashboard.deletecategory');
+        });
+        Route::controller(PostController::class)->group(function(){
+            Route::get('/newpost','create')->name('newpost');
+            Route::post('/newpost','store')->name('storepost');
+            Route::get('/posts','showbyUser')->name('showpostbyuser');
+            Route::delete('/posts/{post}','destroy')->name('deletepost');
+            Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('postedit');
+            Route::put('/posts/{post}', [PostController::class, 'update'])->name('postupdate');
+    
+        });
+        Route::controller(PageController::class)->group(function(){
+            Route::get('/newpage','create')->name('newpage');
+            Route::get('/pages','showbyuser')->name('showpagesbyuser');
+            Route::post('/newpage','store')->name('storepage');
+            Route::delete('/pages/{page}','destroy')->name('deletepage');
+            Route::get('/pages/{page}/edit', 'edit')->name('pageedit');
+            Route::put('/pages/{page}', 'update')->name('updatepage');
+    
+    
+        });
+        Route::get('/comments',[CommentController::class,'index'])->name('managecomments');
+        Route::post('/comments/{comment}',[CommentController::class,'action'])->name('actioncomment');
+    
+    
+        Route::get('/registeruser',[RegisteredUser::class,'create'])->name('createuser')->middleware('can:admin');
+        Route::post('/registeruser',[RegisteredUser::class,'createtheuser'])->name('createtheuser')->middleware('can:admin');
+    
+    
+    }); 
+});
 
 Route::post('comments/store', [CommentController::class, 'store'])->name('comments.store')->middleware('auth');
 Route::get('pages/{url}',[PageController::class,'show'])->name('showpage');
